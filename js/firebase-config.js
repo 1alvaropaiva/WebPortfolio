dfcde2b59c7aaa5eec2efe35e091a6e67eb78d27
1alvaropaiva <alvaropaiva.dev@gmail.com>
2025-10-01 15:16:06 -0300

    add: firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,27 +1,27 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBkFp-Gri-69zayaCwqFBLtWDGHAHWnBZI",
-  authDomain: "portfolio-web-26593.firebaseapp.com",
-  projectId: "portfolio-web-26593",
-  storageBucket: "portfolio-web-26593.firebasestorage.app",
-  messagingSenderId: "807097996914",
-  appId: "1:807097996914:web:7d01bd999b886c7b1014eb"
+    apiKey: "AIzaSyChno4wU3q2Zkuivw6xXszUC_otw_-YrdE",
+    authDomain: "portfolio-dee26.firebaseapp.com",
+    projectId: "portfolio-dee26",
+    storageBucket: "portfolio-dee26.firebasestorage.app",
+    messagingSenderId: "77213621627",
+    appId: "1:77213621627:web:9f78b5ea7848a4b6aa71de"
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
 async function sendContactMessage(name, email, message) {
-  try {
-    const docRef = await db.collection("contacts").add({
-      name: name,
-      email: email,
-      message: message,
-      timestamp: new Date()
-    });
-    console.log("Mensagem enviada com ID: ", docRef.id);
-    return true;
-  } catch (error) {
-    console.error("Erro ao enviar mensagem: ", error);
-    return false;
-  }
+    try {
+        const docRef = await db.collection("contacts").add({
+            name: name,
+            email: email,
+            message: message,
+            timestamp: new Date()
+        });
+        console.log("Mensagem enviada com ID: ", docRef.id);
+        return true;
+    } catch (error) {
+        console.error("Erro ao enviar mensagem: ", error);
+        return false;
+    }
 }
